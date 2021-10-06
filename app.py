43cd36ce34702458ff18270ffbfcfa05b981d500
Kent-Probe <90918251+Kent-Probe@users.noreply.github.com>
2021-10-06 16:51:17 -0500

    6/10/2021

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
-
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/home/")
+@app.route("/index/")
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route("/login/")
+def login():
+    return render_template('login.html')
+
+@app.route("/sign_in/")
+def sign():
+    return render_template('sign_in.html')
+
+@app.route("/username/")
+def username():
+    return 'username'
+
+@app.route("/favorites/")
+def favorites():
+    return 'favorites'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
