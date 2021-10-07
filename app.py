20b6964bcbf4612a0ec71c89fef9ff8cfdb00415
Kent-Probe <90918251+Kent-Probe@users.noreply.github.com>
2021-10-07 15:01:37 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash
+from markupsafe import escape
+from wtforms import form
+from forms import Login, sign_in
+import os
+
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route("/home/")
 @app.route("/index/")
@@ -7,14 +13,38 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/login/")
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    frm = Login()
+    if request.method=='GET':
+        return render_template('login.html', form=frm, titulo='Ingresar')
+    else:
+        # 1. Recuperar los datos del formulario y le aplico transformaciones
+        usr = frm.usr.data.strip()
+        pwd = frm.pwd.data.strip()
+        # 2. Validar (Siempre es una buena práctica)
+        sw = True
+        if len(usr)<5 or len(usr)>40:
+            flash("El nombre de usuario es requerido, longitud no valida [5-40]")
+            sw = False
+        if len(pwd)<5 or len(pwd)>40:
+            flash("El password es requerido, longitud no valida [5-40]")
+            sw = False
+        # 3. Ejecutar la acción - Login imposturado (simular)
+        if sw and usr=='pedro' and pwd=='pedro':
+            session['usuario'] = usr
+            session['clave'] = pwd
+            return render_template("home.html")
+        else:
+            flash("Usuario o contraseña no validos")
+            return render_template('login.html', form=frm, titulo='Ingresar')
+
+
 
 @app.route("/sign_in/")
 def sign():
-    #Recall you have to send the template along with the formulary class (must be imported previously)
-    return render_template('sign_in.html')
+    form = sign_in
+    return render_template('sign_in.html', form=form, titulo='Registro')
 
 @app.route("/username/")
 def username():
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, TextField
+from wtforms import PasswordField, SubmitField, TextField, IntegerField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired, Length, EqualTo
 
@@ -10,9 +10,12 @@ class Login(FlaskForm):
     btn = SubmitField('Ingresar')
 
 class sign_in(FlaskForm):
-    nom = TextField('Nombre*',validators=[Length(min=1, max=100, message='Longitud fuera de rango'),InputRequired(message='Nombre es requerido')])
+    nom = TextField('Nombres*',validators=[Length(min=1, max=100, message='Longitud fuera de rango'),InputRequired(message='Nombre es requerido')])
+    apl = TextField('Apellidos*',validators=[Length(min=1, max=100, message='Longitud fuera de rango'),InputRequired(message='Nombre es requerido')])
     ema = EmailField('Email*',validators=[Length(min=3, max=100, message='Longitud fuera de rango'),InputRequired(message='Email es requerido')])
+    vEm = EmailField('verificacion de Email*',validators=[Length(min=3, max=100, message='Longitud fuera de rango'),InputRequired(message='Email es requerido'),  EqualTo(ema, message='La clave y su verificación no corresponden')])
     usr = TextField('Usuario*',validators=[Length(min=5, max=40, message='Longitud fuera de rango'),InputRequired(message='Usuario es requerido')])
+    num = IntegerField('Numero*',validators=[InputRequired(message='El numero es requerido')])
     cla = PasswordField('Clave*',validators=[Length(min=5, max=40, message='Longitud fuera de rango'),InputRequired(message='Clave es requerido')])
-    ver = PasswordField('Verificación*',validators=[Length(min=5, max=40, message='Longitud fuera de rango'),InputRequired(message='Clave es requerido'), EqualTo(cla, message='La clave y su verificación no corresponden')])
+    ver = PasswordField('Verificación de la clave*',validators=[Length(min=5, max=40, message='Longitud fuera de rango'),InputRequired(message='Clave es requerido'), EqualTo(cla, message='La clave y su verificación no corresponden')])
     btn = SubmitField('Registrar')
