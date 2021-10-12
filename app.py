450196312e79baef49d58328daa672792d4d9b6d
Kent-Probe <90918251+Kent-Probe@users.noreply.github.com>
2021-10-12 09:22:51 -0500

    12/10/2021

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, session, flash
 from markupsafe import escape
 from wtforms import form
-from forms import Login, sign_in, search, comentar
+from forms import Login, sign_in, search, comentar, plate
 import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+#Pagina de inicio
 @app.route("/Principal/")
 @app.route("/home/")
 @app.route("/index/")
@@ -14,12 +15,14 @@ app.secret_key = os.urandom(24)
 def index():
     return render_template('index.html')
 
+#Pagina de inicar sesion
 @app.route('/Entrar/', methods=['GET', 'POST'])
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     form = Login()
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form, titulo='Ingresar')
 
+#Pagina de registrarse
 @app.route("/Registrarse/")
 @app.route("/sign_in/")
 def sign():
@@ -35,16 +38,19 @@ def sign():
     btn = form.btn;
     return render_template('sign_in.html', form=form, titulo='Registro')
 
+#Pagina de usuario
 @app.route("/usuario/")
 @app.route("/username/")
 def username():
-    return 'username'
+    return render_template('usuarios.html', titulo='usuario')
 
+#Pagina de favoritos
 @app.route("/favoritos/")
 @app.route("/favorites/")
 def favorites():
     return 'favorites'
 
+#Pagina de buscar - Todos los productos
 @app.route("/search/")
 @app.route("/buscar/")
 @app.route("/todos_los_Productos/")
@@ -54,10 +60,12 @@ def all_product():
     seh = form.seh
     return render_template('all_product.html', form=form, titulo='producto')
 
+#Pagina de caro de compras
 @app.route("/shopping_car/")
 def shopping_car():
     return render_template('shopping_car.html', form=form, titulo='Carro de compras')
 
+#Pagina de producto en especifico
 @app.route("/producto/<string:name>/")
 @app.route("/producto/")
 def product(name="cafe del bueno"):
@@ -65,14 +73,38 @@ def product(name="cafe del bueno"):
     coment = form.coment
     return render_template('product.html', form=form, titulo='producto')
 
-@app.route('/platos')
+#Pagina de dashboard platos
+@app.route('/dashboard/')
+@app.route('/dashboard/platos/')
 def platos():
     form = search()
-    return render_template('platos.html', form=form)
+    return render_template('platos.html', form=form, titulo='dashboard')
 
-@app.route('/usuarios')
+#Pagina de dashboard gestion usuario
+@app.route('/dashboard/usuarios/')
 def usuarios():
-    return render_template('usuarios.html')
+    return render_template('usuarios.html', titulo='dashboard')
+
+#Pagina agregar platos
+@app.route('/dashboar/add_plate/')
+@app.route('/dashboar/agregar_platos/')
+def addPlatos():
+    form = plate()
+    nPlato = form.nPlato;
+    pPlato = form.pPlato;
+    dPlato = form.dPlato;
+    aImgPlato = form.aImgPlato;
+    return render_template('add_plate.html', form=form, titulo='dashboard')
+
+@app.route('/dashboar/edit_plate/')
+@app.route('/dashboar/editar_platos/')
+def editPlato():
+    form = plate()
+    nPlato = form.nPlato;
+    pPlato = form.pPlato;
+    dPlato = form.dPlato;
+    aImgPlato = form.aImgPlato;
+    return render_template('edit_plate.html', form=form, titulo='dashboard')
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
-from os import name
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField, TextField, IntegerField, TextAreaField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.widgets.core import TextArea
 
 class Login(FlaskForm):
     name = TextField('Usuario*',validators=[Length(min=5, max=40, message='Longitud fuera de rango'),InputRequired(message='Usuario es requerido')])
@@ -30,3 +30,9 @@ class search(FlaskForm):
 
 class comentar(FlaskForm):
     coment = TextAreaField('Comentario')
+
+class plate(FlaskForm):
+    pPlato = TextField('precio del plato')
+    nPlato = TextField('Nombre del plato')
+    dPlato = TextAreaField('Descripcion de plato')
+    aImgPlato = TextAreaField('Agregar imagen')
